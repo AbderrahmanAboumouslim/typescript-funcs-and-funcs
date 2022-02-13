02691a2b157cf26c4d99e79aaadb2c729241130e
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-02-13 14:36:39 +0100

    func closure & func overloading

--- funcOne.ts
+++ funcOne.ts
@@ -1,5 +1,46 @@
-export const func = (
-  arryaOfnumbers: number[],
-  y: (a: number) => number
-): number[] => arryaOfnumbers.map(y);
+type Callback = (a: number) => number;
+
+export const func = (arryaOfnumbers: number[], callback: Callback): number[] =>
+  arryaOfnumbers.map(callback);
+
 console.log(func([11, 33, 44], (d) => d * 3));
+
+const littleCallback: Callback = (a) => a / 2;
+console.log(littleCallback(1));
+
+// func return func ( closures)
+export const funcy =
+  (a: number): Callback =>
+  (b: number) =>
+    a + b;
+const finder = funcy(10);
+console.log(finder(20));
+
+////////////////////////////////
+///// function OVERLOADING ////
+//////////////////////////////
+interface MyObject {
+  x: number;
+  y: number;
+}
+
+// function getDataA(obj: MyObject): MyObject;
+// function getDataB(a: number, b: number): MyObject;
+function getDataC(paramOne: unknown, paramTwo?: unknown): MyObject {
+  let data: MyObject = {
+    x: 0,
+    y: 0,
+  };
+
+  if (typeof paramOne === "object") {
+    data = { ...(paramOne as MyObject) };
+  } else {
+    data = {
+      x: paramOne as number,
+      y: paramTwo as number,
+    };
+  }
+
+  return data;
+}
+console.log(getDataC({ x: 4, y: 5 }));
