2bdc2fc1f5b1795c3c33298e27a4113885021aec
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-02-16 22:48:22 +0100

    typescript use reduce as a map()

--- generics/challenge-1.ts
+++ generics/challenge-1.ts
@@ -17,3 +17,9 @@ const generatorFilter = <T>(items: T[], filterFunc: (x: T) => boolean): T[] =>
   items.reduce((a: T[], b) => (filterFunc(b) ? [...a, b] : a), []);
 
 console.log(generatorFilter([1, 44, 2, 33, 10, 5, 100], (x) => x >= 10));
+
+// Map
+const generatorMap = <T>(items: T[], mapFunc: (x: T) => T): T[] =>
+  items.reduce((a: T[], b) => [...a, mapFunc(b)], []);
+
+console.log(generatorMap([1, 2, 3, 4, 5], (x) => x + 2));
