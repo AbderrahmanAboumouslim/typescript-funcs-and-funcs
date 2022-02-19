6c7afda9c5355ecfc414515418eb2912b146b118
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-02-19 23:15:55 +0100

    utility-types override

---
+++ utility-types/ex-1.ts
@@ -0,0 +1,22 @@
+interface Dev {
+  name: string;
+  age: number;
+  email: string;
+}
+
+interface Override {
+  name?: string;
+  age?: number;
+  email?: string;
+}
+// override
+const generateDev = (dev: Dev, override: Override): Dev => {
+  return { ...dev, ...override };
+};
+
+console.log(
+  generateDev(
+    { name: "Abderrahamn", age: 25, email: "abcd@ok.com" },
+    { email: "myEmail@youEmail.com" }
+  )
+);
